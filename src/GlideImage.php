f251433308ea3802e066717513a2082a566a5917
JolitaGrazyte <jolita.grazyte@student.kdg.be>
2016-05-05 17:10:33 +0200

    made possible to add a watermark (#54)
    fix support for watermarks

--- src/GlideImage.php
+++ src/GlideImage.php
@@ -4,6 +4,8 @@ namespace Spatie\Glide;
 
 use League\Glide\ServerFactory;
 use Spatie\Glide\Exceptions\SourceFileDoesNotExist;
+use League\Flysystem\Filesystem;
+use League\Flysystem\Adapter\Local;
 
 class GlideImage
 {
@@ -14,8 +16,8 @@ class GlideImage
 
     /**
      * @var array The modification the need to be made on the image.
-     * Take a look at Glide's image API to see which parameters are possible.
-     * http://glide.thephpleague.com/1.0/api/quick-reference/
+     *            Take a look at Glide's image API to see which parameters are possible.
+     *            http://glide.thephpleague.com/1.0/api/quick-reference/
      */
     protected $modificationParameters = [];
 
@@ -48,16 +50,38 @@ class GlideImage
 
         $cacheDir = sys_get_temp_dir();
 
+        if (array_has($this->modificationParameters, 'mark')) {
+            list($watermarksFolder, $modificationParameters) = $this->getWatermarkParameters();
+        }
+
         $glideServer = ServerFactory::create([
             'source' => dirname($this->sourceFile),
             'cache' => $cacheDir,
             'driver' => config('laravel-glide.driver'),
+            'watermarks' => !empty($watermarksFolder) ? new Filesystem(new Local($watermarksFolder)) : null,
         ]);
 
-        $conversionResult = $cacheDir.'/'.$glideServer->makeImage($sourceFileName, $this->modificationParameters);
+        $conversionResult = $cacheDir.'/'.$glideServer->makeImage($sourceFileName, $modificationParameters ?? $this->modificationParameters);
 
         rename($conversionResult, $outputFile);
 
         return $outputFile;
+    }
+
+    protected function getWatermarkParameters() : array
+    {
+        $pathParts = pathinfo($this->modificationParameters['mark']);
+
+        $modificationParameters = collect($this->modificationParameters)->map(function ($item, $key) use ($pathParts) {
+
+            if ($key === 'mark') {
+                $item = $pathParts['basename'];
+            }
+
+            return $item;
+
+        })->toArray();
+
+        return [$pathParts['dirname'], $modificationParameters];
     }
 }
--- tests/Integration/GlideImageTest.php
+++ tests/Integration/GlideImageTest.php
@@ -7,9 +7,7 @@ use Spatie\Glide\GlideImage;
 
 class GlideImageTest extends TestCase
 {
-    /**
-     * @test
-     */
+    /** @test */
     public function it_can_modify_an_image()
     {
         $targetFile = __DIR__.'/temp/conversion.jpg';
@@ -21,9 +19,7 @@ class GlideImageTest extends TestCase
         $this->assertFileExists($targetFile);
     }
 
-    /**
-     * @test
-     */
+    /** @test */
     public function it_can_copy_a_file()
     {
         $targetFile = __DIR__.'/temp/conversion.jpg';
@@ -34,13 +30,28 @@ class GlideImageTest extends TestCase
         $this->assertFileExists($targetFile);
     }
 
-    /**
-     * @test
-     */
+    /** @test */
     public function it_will_throw_an_exception_if_the_source_file_does_not_exist()
     {
         $this->setExpectedException(SourceFileDoesNotExist::class);
 
         GlideImage::create('blabla');
+    }
+
+    /** @test */
+    public function it_can_add_a_watermark_to_an_image()
+    {
+        $targetFile = __DIR__.'/watermarked/conversion.jpg';
+        $watermark = __DIR__.'/watermarks/watermark.png';
+
+        GlideImage::create($this->getTestJpg())
+        ->modify(['mark' => $watermark, 'markw' => '40', 'markh' => '40', 'markpad' => '15', 'markpos' => 'top-right'])
+        ->save($targetFile);
+
+        $targetImageHash = hash_file('sha1', $targetFile);
+        $testImageHash = hash_file('sha1', $this->getTestJpg());
+
+        $this->assertFileExists($targetFile);
+        $this->assertNotEquals($targetImageHash, $testImageHash);
     }
 }
